ba9b6d64c73163fea3bce62f3bc82d60101fc900
Zelchi <tininhakawaii@gmail.com>
2024-09-06 19:35:58 -0300

    Express

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 const ROTAS = {
     "/": "Curso de Node.js",
     "/livros": "Entrei na rota livros",
-    "/batata": "Batatinha frita",
+    "/batata": "Entrei na rota batata",
 };
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(ROTAS[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('Servidor Online!');
 });
